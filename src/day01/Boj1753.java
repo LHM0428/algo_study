f700074ac896b4d09d7563ac6112dbe74da86b79
lhm0428 <lhm3031@hanmail.net>
2020-10-25 13:05:59 +0900

    day1 문제 제출

---
+++ src/day01/Boj1753.java
@@ -0,0 +1,97 @@
+package day01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Boj1753 {
+	static class Scan{
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public Scan() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		public String next(){
+			while(st == null || !st.hasMoreTokens()){
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			return st.nextToken();
+		}
+		
+		public int nextInt(){
+			return Integer.parseInt(next());
+		}
+	}
+	
+	static class Vertice{
+		int next;
+		int weight;
+		public Vertice(int next, int weight) {
+			super();
+			this.next = next;
+			this.weight = weight;
+		}
+	}
+	static int V,E,TOP;
+	static List<Vertice>[] vertices;
+	static int[] D;
+	static final int MAX = 3000001;
+	public static void main(String[] args) {
+		Scan sc = new Scan();
+		
+		V = sc.nextInt();
+		E = sc.nextInt();
+		TOP = sc.nextInt();
+		
+		vertices = new ArrayList[V+1];
+		D = new int[V+1];
+		
+		for(int i=1; i<=V; i++){
+			vertices[i] = new ArrayList<>();
+			D[i] = MAX;
+		}
+		
+		for(int i=0; i<E; i++){
+			vertices[sc.nextInt()].add(new Vertice(sc.nextInt(), sc.nextInt()));
+		}
+		
+		PriorityQueue<Vertice> pq = new PriorityQueue<>(new Comparator<Vertice>() {
+			@Override
+			public int compare(Vertice o1, Vertice o2) {
+				return o1.weight - o2.weight;
+			}
+		});
+		
+		D[TOP] = 0;
+		
+		pq.add(new Vertice(TOP, 0));
+		
+		while(!pq.isEmpty()){
+			Vertice v = pq.poll();
+			
+			for(Vertice next : vertices[v.next]){
+				if(D[next.next] > D[v.next] + next.weight){
+					D[next.next] = D[v.next] + next.weight;
+					pq.add(new Vertice(next.next, D[next.next]));
+				}
+			}
+		}
+		
+		for(int i=1; i<=V; i++){
+			if(D[i] == MAX) System.out.println("INF");
+			else System.out.println(D[i]);
+		}
+	}
+}
---
+++ src/day01/Boj2644.java
@@ -0,0 +1,82 @@
+package day01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Boj2644 {
+	static class Scan{
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public Scan() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		public String next(){
+			while(st == null || !st.hasMoreTokens()){
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			return st.nextToken();
+		}
+		
+		public int nextInt(){
+			return Integer.parseInt(next());
+		}
+	}
+	
+    static int n, personA, personB, m;
+	static int[][] relations;
+	static int[] dist;
+	public static void main(String[] args) {
+		Scan sc = new Scan();
+		
+		
+		n = sc.nextInt();
+		personA = sc.nextInt();
+		personB = sc.nextInt();
+		m = sc.nextInt();
+		
+		dist = new int[n+1];
+		relations = new int[n+1][n+1];
+		for(int i=0; i<=n; i++) {
+			Arrays.fill(dist, -1);
+			
+		}
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		
+		for(int i=0; i<m; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			relations[a][b] = 1;
+			relations[b][a] = 1;
+		}
+		q.add(personA);
+		dist[personA] = 0;
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			
+			if(now == personB) break;
+			
+			for(int next=1; next<=n; next++) {
+				if(relations[now][next] > 0 && dist[next] < 0 ) {
+					dist[next] = dist[now] + 1;
+					q.add(next);
+				}
+			}
+		}
+		System.out.println(dist[personB]);
+	}
+}
